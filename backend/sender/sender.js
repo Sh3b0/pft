060fc7f2d6cbd046bb33410c13088ac37c05918d
sh3b0 <a.shaaban@innopolis.university>
2021-09-01 00:47:59 +0300

    File transfer 101

--- backend/sender/sender.js
+++ backend/sender/sender.js
@@ -1,39 +1,88 @@
-function formSubmit(event) {
-    event.preventDefault()
-    console.log(event)
-    const answer = document.getElementById("answer").value
-    let tmp = JSON.parse(answer)
-    let ans = {}
-    ans['type'] = tmp.type
-    ans['sdp'] = tmp.sdp
-    localConnection.setRemoteDescription(ans).then(() => console.log("Done"))
-}
+// Sender code
 
+// Globals
 const form = document.getElementById('form')
-form.addEventListener('submit', formSubmit)
+const fileInput = document.getElementById('fileInput')
+const sendButton = document.getElementById('sendButton')
+let localConnection, sendChannel
 
 
-const conf = {}
-conf.iceServers = []
+// Creating local connection
+window.onload = e => {
+    console.log('Document loaded: ', e)
+    const conf = {}
+    conf.iceServers = []
+    conf.iceServers.push({
+        urls: 'stun:stun1.l.google.com:19302'
+    })
 
-// stun server
-conf.iceServers.push({
-    urls: 'stun:stun1.l.google.com:19302'
-})
+    localConnection = new RTCPeerConnection(conf)
+    localConnection.onicecandidate = e => {
+        console.log("New ice candidate", e)
+        const offer = localConnection.localDescription
+        const offerTxt = document.getElementById('offer')
+        offerTxt.innerHTML = JSON.stringify(offer)
+    }
 
-const localConnection = new RTCPeerConnection(conf)
+    sendChannel = localConnection.createDataChannel("sendChannel")
+    sendChannel.onmessage = e => console.log("Message from peer: " + e.data)
+    sendChannel.onopen = () => console.log("Channel opened")
+    sendChannel.onclose = () => console.log("Channel closed")
 
-localConnection.onicecandidate = () => {
-    console.log("New ice candidate")
-    const offer = localConnection.localDescription
-    const offerTxt = document.getElementById('offer')
-    offerTxt.innerHTML = JSON.stringify(offer)
+    localConnection.createOffer().then(o => localConnection.setLocalDescription(o))
+}
+
+// Toggle send button
+fileInput.onchange = e => {
+    console.log('File input changed')
+    sendButton.disabled = !fileInput.files.length;
+}
+
+// Send button functionality
+sendButton.onclick = e => {
+    console.log('Sending file: ' + e)
+    sendButton.disabled = true
+
+    if(sendChannel.readyState !== 'open'){
+        console.log('Data Channel is not ready')
+        return
+    }
+
+    const file = fileInput.files[0]
+    const fileReader = new FileReader();
+    const chunkSize = 16384
+    let so_far = 0
+
+    fileReader.onerror = e => {
+        console.error('Error reading file: ', e)
+    }
+    fileReader.onabort = e => {
+        console.log('Aborting: ', e)
+    }
+
+    // On reading a part of the file, we send that part
+    fileReader.onload = e => {
+        sendChannel.send(e.target.result);
+        so_far += e.target.result.byteLength;
+        if (so_far < file.size) {
+            readPart(so_far);
+        }
+    }
+
+    // Reads chunkSize bytes of the file, starting from byte o
+    const readPart = o => {
+        console.log(`Reading chunk, starting from `, o);
+        const slice = file.slice(o, o + chunkSize);
+        fileReader.readAsArrayBuffer(slice);
+    };
+    readPart(0);
 }
 
 
-const sendChannel = localConnection.createDataChannel("sendChannel")
-sendChannel.onmessage = e => console.log("Message from peer: " + e.data)
-sendChannel.onopen = () => console.log("Channel opened")
-sendChannel.onclose = () => console.log("Channel closed")
-
-localConnection.createOffer().then(o => localConnection.setLocalDescription(o))
+// Temporary method of connection establishment
+// TODO: create rooms system
+form.onsubmit = e => {
+    e.preventDefault()
+    const answer = document.getElementById("answer").value
+    localConnection.setRemoteDescription(JSON.parse(answer)).then(() => console.log("Connected to receiver"))
+}
