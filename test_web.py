70580ee6000c168a46e74f5206306c6a37421836
TalgatBektleuov <t.bektleuov@innopolis.university>
2021-10-10 17:27:23 +0300

    changed url

--- test_web.py
+++ test_web.py
@@ -7,7 +7,7 @@ from seleniumbase import BaseCase
 class UploadTest(BaseCase):
 
     def test_visible_upload(self):
-        self.open("http://127.0.0.1:5000/#")
+        self.open("http://pipeft.herokuapp.com")
 
         file_path = "./test_files/pdf-test.pdf"
         self.choose_file("#file-input", file_path)
@@ -15,12 +15,10 @@ class UploadTest(BaseCase):
         code = self.get_text('#invite-link')
 
         self.open_new_window()
-        self.open("http://127.0.0.1:5000/r/")
+        self.open("http://pipeft.herokuapp.com/r/")
         self.type("#invite-link", code)
         self.click("#connect-button")
         sleep(3)
         # download_message = self.get_text("#is-received1")
         # print("This is a message: ", download_message)
         self.assert_text("Downloaded", "#is-received1")
-
-
