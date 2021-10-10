eefe7393e7074e8d737e78ad6e39fa5cc23df8bd
TalgatBektleuov <t.bektleuov@innopolis.university>
2021-10-10 17:39:28 +0300

    minor changes

--- test_web.py
+++ test_web.py
@@ -1,7 +1,6 @@
 from time import sleep
+
 from seleniumbase import BaseCase
-# from selenium import webdriver
-# from selenium.webdriver.support.ui import WebDriverWait
 
 
 class UploadTest(BaseCase):
@@ -19,6 +18,5 @@ class UploadTest(BaseCase):
         self.type("#invite-link", code)
         self.click("#connect-button")
         sleep(3)
-        # download_message = self.get_text("#is-received1")
-        # print("This is a message: ", download_message)
+
         self.assert_text("Downloaded", "#is-received1")
